a7c440c77ed1a712953f9b67da3b919663492a71
Aditya Subramanian <subramanianaditya42@gmail.com>
2020-05-24 20:36:34 +0200

    Unique-fy bookmarks

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,16 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import GlobalContext from "../state/GlobalContext";
+import { isDuplicate } from "../utils/helperFunctions";
 
 const Bookmarks = () => {
   const { state, dispatch } = useContext(GlobalContext);
   const { bookmarks, currentVideo } = state;
+  const [bookmarkCount, setBookmarkCount] = useState(0);
 
+  const isBookmarkDuplicate = (videoId, bookmarkArr) => {
+    const idArr = bookmarkArr.map((item) => item.videoId);
+    console.log(isDuplicate(videoId, idArr));
+    return isDuplicate(videoId, idArr);
+  };
   const addToBookmark = () => {
-    const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
-    console.log("bookmarks :>> ", bookmarks);
-    bookmarks.push(currentVideo);
-    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
-    dispatch({ type: "addToBookmarks" });
+    const localStorageBookmarks =
+      JSON.parse(localStorage.getItem("bookmarks")) || [];
+    if (!isBookmarkDuplicate(currentVideo.videoId, localStorageBookmarks)) {
+      localStorageBookmarks.push(currentVideo);
+      dispatch({ type: "addToBookmarks" });
+      localStorage.setItem("bookmarks", JSON.stringify(localStorageBookmarks));
+      setBookmarkCount(localStorageBookmarks.length);
+    }
   };
 
   return (
@@ -20,7 +30,7 @@ const Bookmarks = () => {
       )}
       <ul>
         {bookmarks.map((item) => (
-          <li key={item.videoId}>
+          <li key={item._id}>
             {item.title} {item.author}
           </li>
         ))}
